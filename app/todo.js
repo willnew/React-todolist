de661633220253fe0a76d6c558975f7603102cd8
will liu <will.liu09525@gmail.com>
2016-06-27 17:36:37 +0800

    simple uI

--- app/todo.js
+++ app/todo.js
@@ -1,9 +1,56 @@
 import React from 'react'
 
-module.exports = React.createClass({
+var TodoItem = React.createClass({
   render() {
     return (
-      <div>TODO List</div>
+      <div>
+        {this.props.itemName}
+      </div>
+    )
+  }
+})
+
+var TodoList = React.createClass({
+  render() {
+    return (
+      <div>
+        <TodoItem itemName="coding"></TodoItem>
+        <TodoItem itemName="self testing"></TodoItem>
+        <TodoItem itemName="fixing bug"></TodoItem>
+      </div>
+    )
+  }
+})
+
+var TodoForm = React.createClass({
+  onTodoFormSubmit(event) {
+    event.preventDefault()
+    console.log(event.target.elements[0].value)
+  },
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.onTodoFormSubmit}>
+          <input type="text" />{''}
+        </form>
+      </div>
+    )
+  }
+})
+
+module.exports = React.createClass({
+  onTodoItemAdd() {
+
+  },
+  onTodoItemDelete() {
+
+  },
+  render() {
+    return (
+      <div>
+        <TodoList></TodoList>
+        <TodoForm></TodoForm>
+      </div>
     );
   }
 });
